db8253a394733882d186237b21adc5be3a9a5ff6
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-19 20:50:51 +0100

    feat: added brekpoint custo to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ export default {
 
     extend: {
 
+      screens: {
+
+        'xs': '422px',
+
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+
+      },
+
       colors: {
 
         customLight: '#FBF4E6',
