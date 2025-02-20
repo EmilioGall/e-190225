70a3cd93e956d65af8834eafffa3178ce57af5d1
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-20 15:16:48 +0100

    feat: updated behaviour of transition for texts - src/components/Biography.jsx

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -3,62 +3,50 @@ import Button from './Button';
 
 export default function Biography() {
 
-   const [showBiography, setShowBiography] = useState(true);
+   const [showBiography, setShowBiography] = useState(false);
 
    const handleButtonClick = () => {
 
-      showBiography ? setShowBiography(false) : setShowBiography(true);
+      setShowBiography(!showBiography);
+
    };
 
    return (
 
-      <section className="flex flex-col xs:items-center text-customLight bg-customDark p-8  xs:px-60">
+      <section className="flex flex-col xs:items-center text-customLight bg-customDark py-8 ">
 
          <h2 className="font-customRoboto font-customNormal text-lg pb-4 xs:pb-8">BIOGRAPHY</h2>
 
-         {
-            showBiography ? (
-               <div>
+         <div className={`flex flex-col xs:items-center px-8 ${showBiography ? 'w-2/3' : ''}`}>
 
-                  <div className="flex flex-col xs:flex-row xs:justify-center gap-8 transition-opacity duration-1000 opacity-100 pb-4">
+            <div className={`flex-col xs:flex-row xs:justify-center gap-8 pb-4 transition-all duration-1000 overflow-hidden ${showBiography ? 'opacity-100 max-h-[1000px]' : 'opacity-0  max-h-0'}`}
+            >
 
-                     <div className="font-customRoboto font-customNormal text-sm xs:w-1/3">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet. Pellentesque dapibus euismod enim feugiat sagittis. Sed ornare suscipit tellus, at convallis ante dapibus eget. Morbi vel augue sed dui ornare pretium eget ut urna. Vestibulum efficitur egestas ex nec euismod. Vestibulum non lacus ante. Aenean quis aliquet justo. Vivamus consectetur rutrum magna vitae semper. Nunc vitae magna lorem.
-                     </div>
-
-                     <div className="font-customRoboto font-customNormal text-sm xs:w-1/3">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet. Pellentesque dapibus euismod enim feugiat sagittis. Sed ornare suscipit tellus, at convallis ante dapibus eget. Morbi vel augue sed dui ornare pretium eget ut urna. Vestibulum efficitur egestas ex nec euismod. Vestibulum non lacus ante. Aenean quis aliquet justo. Vivamus consectetur rutrum magna vitae semper. Nunc vitae magna lorem.
-                     </div>
-
-                  </div>
-
-                  <div>
-
-                     <Button arrow={false} text={'LESS'} functionOnClick={handleButtonClick} />
-
-                  </div>
-
+               <div className="font-customRoboto font-customNormal text-sm">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet. Pellentesque dapibus euismod enim feugiat sagittis. Sed ornare suscipit tellus, at convallis ante dapibus eget. Morbi vel augue sed dui ornare pretium eget ut urna. Vestibulum efficitur egestas ex nec euismod. Vestibulum non lacus ante. Aenean quis aliquet justo. Vivamus consectetur rutrum magna vitae semper. Nunc vitae magna lorem.
                </div>
 
-            ) : (
-
-               <div>
-
-
-                  <p className="font-customRoboto font-customNormal text-sm pb-4 xs:w-1/3 transition-opacity duration-1000 opacity-100">
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet.
-                  </p>
-
-                  <div>
-
-                     <Button arrow={false} text={'READ MORE'} functionOnClick={handleButtonClick} />
-
-                  </div>
+               <div className="font-customRoboto font-customNormal text-sm">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet. Pellentesque dapibus euismod enim feugiat sagittis. Sed ornare suscipit tellus, at convallis ante dapibus eget. Morbi vel augue sed dui ornare pretium eget ut urna. Vestibulum efficitur egestas ex nec euismod. Vestibulum non lacus ante. Aenean quis aliquet justo. Vivamus consectetur rutrum magna vitae semper. Nunc vitae magna lorem.
                </div>
-            )
-         }
 
+            </div>
 
+            <div className={`xs:w-1/3 transition-all duration-1000 overflow-hidden ${showBiography ? 'opacity-0 max-h-0' : 'opacity-100 max-h-[1000px]'}`}>
+
+               <p className="font-customRoboto font-customNormal text-sm pb-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet.
+               </p>
+
+            </div>
+
+            <div className={`z-20 ${showBiography ? 'self-end' : ''}`}>
+
+               <Button arrow={false} text={showBiography ? 'LESS' : 'READ MORE'} functionOnClick={handleButtonClick} />
+
+            </div>
+
+         </div>
 
       </section>
 
