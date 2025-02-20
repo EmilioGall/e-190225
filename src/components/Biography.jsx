4ff51e4541d228ebb7bb61a69619de33dba65565
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-20 16:24:38 +0100

    feat: adjusted behaviour of text transation - src/components/Biography.jsx

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -17,9 +17,9 @@ export default function Biography() {
 
          <h2 className="font-customRoboto font-customNormal text-lg pb-4 xs:pb-8">BIOGRAPHY</h2>
 
-         <div className={`flex flex-col xs:items-center px-8 ${showBiography ? 'w-2/3' : ''}`}>
+         <div className={`relative flex flex-col xs:items-center px-8 transition-all delay-1000 ${showBiography ? 'w-2/3' : 'w-full'}`}>
 
-            <div className={`flex-col xs:flex-row xs:justify-center gap-8 pb-4 transition-all duration-1000 overflow-hidden ${showBiography ? 'opacity-100 max-h-[1000px]' : 'opacity-0  max-h-0'}`}
+            <div className={`flex justify-center gap-8 pb-4 transition-all overflow-hidden ${showBiography ? 'delay-1000 duration-500 opacity-100 max-h-full' : 'absolute duration-1000 opacity-0 max-h-0'}`}
             >
 
                <div className="font-customRoboto font-customNormal text-sm">
@@ -32,7 +32,7 @@ export default function Biography() {
 
             </div>
 
-            <div className={`xs:w-1/3 transition-all duration-1000 overflow-hidden ${showBiography ? 'opacity-0 max-h-0' : 'opacity-100 max-h-[1000px]'}`}>
+            <div className={`xs:w-1/3 transition-all duration-1000 overflow-hidden ${showBiography ? 'absolute duration-1000 opacity-0 max-h-0' : 'delay-1000 duration-200 opacity-100 max-h-full'}`}>
 
                <p className="font-customRoboto font-customNormal text-sm pb-4">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet.
