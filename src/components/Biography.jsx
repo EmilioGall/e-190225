0651aa988a8e454c3cfc4ecf8c3ffe54060f8882
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-20 16:46:01 +0100

    feat: adjusted animation behaviour for test toogle - src/components/Biography.jsx

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -17,34 +17,45 @@ export default function Biography() {
 
          <h2 className="font-customRoboto font-customNormal text-lg pb-4 xs:pb-8">BIOGRAPHY</h2>
 
-         <div className={`relative flex flex-col xs:items-center px-8 transition-all delay-1000 ${showBiography ? 'w-2/3' : 'w-full'}`}>
+         <div className={`relative flex flex-col xs:items-center px-8 transition-all delay-700 ${showBiography ? 'w-2/3' : 'w-full'}`}>
 
-            <div className={`flex justify-center gap-8 pb-4 transition-all overflow-hidden ${showBiography ? 'delay-1000 duration-500 opacity-100 max-h-full' : 'absolute duration-1000 opacity-0 max-h-0'}`}
-            >
+            <div className={`flex flex-col transition-all ${showBiography ? 'delay-700 duration-1000 opacity-100' : 'absolute duration-700 opacity-0 '}`}>
 
-               <div className="font-customRoboto font-customNormal text-sm">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet. Pellentesque dapibus euismod enim feugiat sagittis. Sed ornare suscipit tellus, at convallis ante dapibus eget. Morbi vel augue sed dui ornare pretium eget ut urna. Vestibulum efficitur egestas ex nec euismod. Vestibulum non lacus ante. Aenean quis aliquet justo. Vivamus consectetur rutrum magna vitae semper. Nunc vitae magna lorem.
+               <div className={`flex justify-center gap-8 pb-4`}
+               >
+
+                  <div className="font-customRoboto font-customNormal text-sm">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet. Pellentesque dapibus euismod enim feugiat sagittis. Sed ornare suscipit tellus, at convallis ante dapibus eget. Morbi vel augue sed dui ornare pretium eget ut urna. Vestibulum efficitur egestas ex nec euismod. Vestibulum non lacus ante. Aenean quis aliquet justo. Vivamus consectetur rutrum magna vitae semper. Nunc vitae magna lorem.
+                  </div>
+
+                  <div className="font-customRoboto font-customNormal text-sm">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet. Pellentesque dapibus euismod enim feugiat sagittis. Sed ornare suscipit tellus, at convallis ante dapibus eget. Morbi vel augue sed dui ornare pretium eget ut urna. Vestibulum efficitur egestas ex nec euismod. Vestibulum non lacus ante. Aenean quis aliquet justo. Vivamus consectetur rutrum magna vitae semper. Nunc vitae magna lorem.
+                  </div>
+
                </div>
 
-               <div className="font-customRoboto font-customNormal text-sm">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet. Pellentesque dapibus euismod enim feugiat sagittis. Sed ornare suscipit tellus, at convallis ante dapibus eget. Morbi vel augue sed dui ornare pretium eget ut urna. Vestibulum efficitur egestas ex nec euismod. Vestibulum non lacus ante. Aenean quis aliquet justo. Vivamus consectetur rutrum magna vitae semper. Nunc vitae magna lorem.
+               <div className={`z-20 self-end`}>
+
+                  <Button arrow={false} text={'LESS'} functionOnClick={handleButtonClick} />
+
                </div>
 
             </div>
 
-            <div className={`xs:w-1/3 transition-all duration-1000 overflow-hidden ${showBiography ? 'absolute duration-1000 opacity-0 max-h-0' : 'delay-1000 duration-200 opacity-100 max-h-full'}`}>
+            <div className={`flex flex-col items-center xs:w-1/3 transition-all ${showBiography ? 'absolute duration-700 opacity-0 ' : 'delay-700 duration-1000 opacity-100'}`}>
 
                <p className="font-customRoboto font-customNormal text-sm pb-4">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque euismod libero, in commodo velit tincidunt at. Donec cursus scelerisque sodales. Vivamus auctor fringilla laoreet. Nulla lacinia metus sed lorem aliquet, nec varius nunc efficitur. Nullam rhoncus a neque aliquam interdum.Suspendisse vehicula blandit nulla nec imperdiet.
                </p>
 
+               <div className={`z-20`}>
+
+                  <Button arrow={false} text={'READ MORE'} functionOnClick={handleButtonClick} />
+
+               </div>
+
             </div>
 
-            <div className={`z-20 ${showBiography ? 'self-end' : ''}`}>
-
-               <Button arrow={false} text={showBiography ? 'LESS' : 'READ MORE'} functionOnClick={handleButtonClick} />
-
-            </div>
 
          </div>
 
