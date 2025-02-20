8eb865ed86f48a569462ff4285b2774890f3fc95
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-20 19:11:48 +0100

    feat: implemented slide box component in expertise section - src/components/Expertise.jsx

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -6,6 +6,7 @@ import bgImage3 from '../assets/img/detlefovermann_0881 1.png';
 import bgImage4 from '../assets/img/editing.png';
 import bgImage5 from '../assets/img/makegusto.png';
 import bgImage6 from '../assets/img/wowmilano.png';
+import SlideBox from './SlideBox';
 
 export default function Expertise() {
 
@@ -13,7 +14,13 @@ export default function Expertise() {
 
    const expertiseData = [
 
-      { img: bgImage1, title: titles[0], order: 'order-1' },
+      {
+         img: bgImage1,
+         title: titles[0],
+         order: 'order-1',
+         hoverText: 'Founder, art director, exhibition coordinator, Director of the Contemporary Art Institute of Lisbon, Portugal, and curator for the Scandinavian Pavilion at the 2023 Venice Art Biennale.',
+         hoverTitle: titles[0],
+      },
       { img: bgImage2, title: titles[1], order: 'order-2 xs:order-3' },
       { img: bgImage3, title: titles[2], order: 'order-3 xs:order-2' },
       { img: bgImage4, title: titles[3], order: 'order-4' },
@@ -30,18 +37,26 @@ export default function Expertise() {
 
          <div className="grid grid-cols-1 xs:grid-cols-3 w-full">
 
+            <SlideBox
+               backgroundImage={expertiseData[0].img}
+               title={expertiseData[0].title}
+               hoverText={expertiseData[0].hoverText}
+               hoverTitle={expertiseData[0].hoverTitle}
+               order={expertiseData[0].order}
+            />
+
             {
-               expertiseData.map((item, index) => (
+               expertiseData.slice(1).map((item, index) => (
 
                   <div
-                     key={index}
-                     className={`w-full h-full aspect-square bg-no-repeat bg-cover bg-center ${item.order}`}
+                     key={index + 1}
+                     className={`w-full h-full aspect-square z-30 bg-no-repeat bg-cover bg-center ${item.order}`}
                      style={{ backgroundImage: `url(${item.img})` }}
                   >
 
-                     <div className="flex items-center justify-center h-full">
+                     <div className="flex flex-col items-center justify-center w-full h-full">
 
-                        <h3 className="font-customRobotoSlab font-customMedium text-xl xs:text-2xl bg-customDark w-1/2 text-white text-center px-0">
+                        <h3 className="font-customRobotoSlab font-customMedium text-xl xs:text-2xl bg-customDark w-2/3  text-white text-center px-1">
 
                            {item.title}
 
