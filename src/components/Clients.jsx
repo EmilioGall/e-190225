9e90c92f7ad4961f824feb84539df7b26d43ad78
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-20 19:43:35 +0100

    feat: created raw structure of Clients component - src/components/Clients.jsx

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 
 export default function Clients() {
 
-   return (
-      
-      <section>
+   const brands = ['LAURENE', 'ITALY', 'VAGUE', 'KLEE', 'KLESS', 'RIK', 'ECO', 'WOOD']
+   const brandString = ` ${brands.join(' - ')} -`;
 
-         <h2 className='bg-slate-600'>Clients</h2>
+   return (
+
+      <section className="flex flex-col items-center text-customLight bg-customDark py-8 overflow-hidden">
+
+         <h2 className="font-customRoboto font-customNormal text-lg text-center pb-4 xs:pb-8">
+            CLIENTS & COLLABORATIONS
+         </h2>
+
+         <div className="w-full overflow-hidden border-2 border-x-0 border-customYellow">
+
+            <div className="font-customRobotoSlab font-customNormal text-lg text-customYellow flex whitespace-nowrap">
+
+               <span className="ps-2">{brandString}</span>
+               <span className="ps-2">{brandString}</span>
+               <span className="ps-2">{brandString}</span>
+
+            </div>
+
+         </div>
 
       </section>
 
