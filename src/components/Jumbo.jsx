c7d7489c41d98446b7b433313eb23a8e8c5e3a28
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-21 09:27:36 +0100

    refactory: adjusted distance between elements in component - src/components/Jumbo.jsx

--- src/components/Jumbo.jsx
+++ src/components/Jumbo.jsx
@@ -7,7 +7,7 @@ export default function Jumbo() {
    return (
 
       <section
-         className="flex flex-col xs:flex-row justify-between gap-10 text-customLight p-8 pt-20 xs:p-60 bg-cover bg-center w-full bg-image-1 xs:bg-image-2 relative"
+         className="flex flex-col xs:flex-row justify-between gap-10 text-customLight p-12 pt-40 xs:p-60 bg-cover bg-center w-full bg-image-1 xs:bg-image-2 relative"
 
       >
 
@@ -22,7 +22,7 @@ export default function Jumbo() {
             Plans are nothing, planning is everything
          </h1>
 
-         <div className="z-10 w-full xs:w-5/12 pb-3 xs:pt-6 flex flex-col gap-10 justify-between">
+         <div className="z-10 w-full xs:w-5/12 xs:pt-6 flex flex-col gap-10 justify-between mb-32">
 
             <div>
 
